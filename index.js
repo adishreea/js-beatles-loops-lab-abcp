defe965c94559f1d6cf42229c248cd3bf816cb5d
Adishree Annabathula <adishree.annabathula@gmail.com>
2018-01-11 17:22:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var musicians = {}
+var instruments = {}
+
+function theBeatlesPlay(){
+  var arr = {}
+  for (var i = 0; i < musicians.length; i++){
+    
+  }
+  return arr
+}
+
+function johnLennonFacts(arrFacts){
+  
+}
