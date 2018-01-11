b95c9465e94c9043bbd14e1fcf4bc541d1134f84
Adishree Annabathula <adishree.annabathula@gmail.com>
2018-01-11 17:57:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var musicians = []
 var instruments = []
-const facts = [  "He was the last Beatle to learn to drive",   
+var facts = [  "He was the last Beatle to learn to drive",   
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice" ]
