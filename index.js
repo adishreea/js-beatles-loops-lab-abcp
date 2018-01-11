33d7f5820462611892f255877fa4e7a5dd0a2a5d
Adishree Annabathula <adishree.annabathula@gmail.com>
2018-01-11 17:56:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var musicians = []
 var instruments = []
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+]
 
 function theBeatlesPlay(musicians, instruments){
   var arr = []
