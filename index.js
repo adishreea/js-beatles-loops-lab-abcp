7d28e7d587f9a08828ec44d76ae9855fe06eb28f
Adishree Annabathula <adishree.annabathula@gmail.com>
2018-01-11 17:58:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var musicians = []
 var instruments = []
-var facts = [  "He was the last Beatle to learn to drive",   
+const facts = [  "He was the last Beatle to learn to drive",   
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice" ]
